a198c2765d65ca295d85c45f23dd36609b15c4fc
JC <jc.alonsome@gmail.com>
2023-01-26 10:31:56 +0100

    jsjsjsjs

--- src/js/addWord.js
+++ src/js/addWord.js
@@ -1,4 +1,4 @@
-import { ALL_WORDS } from './constants.js';
+import { ALL_WORDS } from "./constants.js";
 
 const randomWord = ALL_WORDS[Math.floor(Math.random() * ALL_WORDS.length)];
 console.log(randomWord);
@@ -6,37 +6,37 @@ let counter = 0;
 
 const paintLetter = (letter, position, className, gameBoard) => {
   const letterBox = gameBoard.children[counter].children[position];
+  if (letterBox.classList.contains("green")) return;
   letterBox.classList.add(className);
   letterBox.textContent = letter;
 };
 
-const printWord = (word, gameBoard) => {
-  let wordToCheck = randomWord;
+const checkWord = (word, gameBoard) => {
+  let wordToCheck = "";
   let className;
   for (let index = 0; index < word.length; index++) {
-    if (randomWord.charAt(index) === word.charAt(index)) {
-      className = 'green';
-      wordToCheck = wordToCheck.replace(word[index], '-');
+    if (word.charAt(index) === randomWord.charAt(index)) {
+      className = "green";
+      wordToCheck += "-";
       paintLetter(word[index], index, className, gameBoard);
+    } else {
+      wordToCheck += randomWord[index];
     }
   }
 
   console.log(wordToCheck);
   for (let index = 0; index < wordToCheck.length; index++) {
-    const letterBox = gameBoard.children[counter].children[index];
     if (wordToCheck.includes(word[index])) {
-      console.log(wordToCheck);
-      className = 'yellow';
-      wordToCheck = wordToCheck.replace(word[index], '-');
+      className = "yellow";
+
       paintLetter(word[index], index, className, gameBoard);
-    } else if (
-      !letterBox.classList.contains('green') ||
-      !letterBox.classList.contains('yellow')
-    )
-      className = 'gray';
-    paintLetter(word[index], index, className, gameBoard);
+    } else {
+      className = "gray";
+      paintLetter(word[index], index, className, gameBoard);
+    }
   }
+
   counter++;
 };
 
-export { printWord };
+export { checkWord as printWord };
